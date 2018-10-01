9640ba726d8089bd6ff15703055404b9392beee7
Olivier MONNIER <monnier@indigo-net.com>
2018-10-01 17:41:58 +0200

    single quotes

--- packages/site/config/autoreload.js
+++ packages/site/config/autoreload.js
@@ -2,13 +2,13 @@ module.exports.autoreload = {
   active: true,
   usePolling: false,
   dirs: [
-    "api/models",
-    "api/controllers",
-    "api/services",
-    "config/locales"
+    'api/models',
+    'api/controllers',
+    'api/services',
+    'config/locales'
   ],
   ignored: [
     // Ignore all files with .ts extension
-    "**.ts"
+    '**.ts'
   ]
 };
