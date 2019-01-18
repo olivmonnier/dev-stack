78221925976037eb57a71903cdb392cc4583eba8
Olivier MONNIER <monnier@indigo-net.com>
2019-01-18 10:33:53 +0100

    fix warning

--- packages/site/api/helpers/create-user.js
+++ packages/site/api/helpers/create-user.js
@@ -1,46 +1,44 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 module.exports = {
-  friendlyName: 'Create user',
-  description: 'Create a new user.',
+  friendlyName: "Create user",
+  description: "Create a new user.",
 
   inputs: {
     username: {
-      type: 'string'
+      type: "string"
     },
     password: {
-      type: 'string'
+      type: "string"
     }
   },
 
   exits: {
     invalid: {
-      responseType: 'badRequest',
-			description: 'The provided email address and/or password are invalid.',
+      description: "The provided email address and/or password are invalid."
     },
     emailAlreadyInUse: {
-			statusCode: 409,
-			description: 'The provided email address is already in use.',
-		},
+      description: "The provided email address is already in use."
+    }
   },
 
   fn: async function(inputs, exists) {
     let attr = {
       // id: sails.helpers.randomCryptoString({ size: 32 }).execSync(),
       username: inputs.username.toLowerCase()
-    }
+    };
 
     if (inputs.password) {
       attr.password = await bcrypt.hash(inputs.password, 10);
 
       const user = await User.create(attr)
-        .intercept('E_UNIQUE', () => 'emailAlreadyInUse')
-        .intercept({ name: 'UsageError' }, () => 'invalid')
+        .intercept("E_UNIQUE", () => "emailAlreadyInUse")
+        .intercept({ name: "UsageError" }, () => "invalid")
         .fetch();
 
       return exists.success(user);
     } else {
-      return exists.invalid('Missing password.')
+      return exists.invalid("Missing password.");
     }
   }
-}
+};
